975f33c7bdbe8c0294d614608d76a5db8b5c576d
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-26 17:33:46 -0400

    create function to copy the string exactly

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,14 @@
 const reverseString = function(input) {
     //create output string
+    let output = '';
 
     //do stuff to output string
+    for(let i = 0; i < input.length; i++){
+        output += input.at(i);
+    }
 
     //return output string
+    return output;
 };
 
 // Do not edit below this line
