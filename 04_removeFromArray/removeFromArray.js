4e75548d4a13ca32ac0a73da46f378f8ac0cc608
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-26 17:58:50 -0400

    add outline to remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function() { //take inputs array and one or more objects
+    //create an array of all objects taken as param EXCLUDING the array
+    const nogoList = []; 
+    //create an output array
+    let outputList = [];
+    //for each object in the array parameter
+    for(const i in arguments[0]){
+        //check against each object in the parameter list
+        //if cleared against the list copy into the output array
+    }
+        
+    //return the output array
+    return outputList;
 };
 
 // Do not edit below this line
