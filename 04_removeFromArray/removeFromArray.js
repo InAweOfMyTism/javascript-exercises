824359e8e753fe40c85364dc634c9a98296e77ca
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-26 18:18:56 -0400

    if statement to copy variable into output array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,24 +2,22 @@ const removeFromArray = function(inputArray, ...severalArguments) { //take input
      
     //create an output array
     let outputList = [];
-    //for each object in the array parameter
+    //for each object in the array input
     for(let i = 0; i < inputArray.length; i++){
-        //create variable to store whether or not it is a match default false
+        //create variable to store whether or not it is a match, default false
         let isMatch = false;
         //check against each object in the parameter list
-            //for each object in noGoList
-        severalArguments.forEach( (compareTarget)=>{
-            if(inputObject == compareTarget){
-                isMatch = true;
-            }
-        });
+        //for each object in noGoList
+        
+        
                 //is it a match?
                     // yes - set match variable to true
                     // no - do nothing
-        if(!isMatch){
-            outputList[]
-        }    
         //if match variable false copy into the output array
+        
+        if(!isMatch){
+            outputList.push(inputArray[i]);
+        }    
     }
         
     //return the output array
