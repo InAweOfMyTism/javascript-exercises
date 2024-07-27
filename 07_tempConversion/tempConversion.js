5c879c043566c33a2cd37be30ee29b4c7242489a
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-27 18:26:17 -0400

    finish ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (5/9)*(tempF - 32);
+  //return tempC.toFixed(1);
+  return Math.round((tempC * 10))/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = 32 + (tempC * 9/5);
+  //return tempF.toFixed(1);
+  return Math.round((tempF * 10))/10;
 };
 
 // Do not edit below this line
