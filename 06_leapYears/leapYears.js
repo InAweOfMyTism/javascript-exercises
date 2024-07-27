0ebd7af98a28d84f25be2688b8b323c77e494b28
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-27 18:14:19 -0400

    finish ex 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+//var to hold value
+let isLeapYear = false;
+//is it div by 400
+if(year % 400 === 0){
+    isLeapYear = true;
+} else if( year % 100 === 0){
+    isLeapYear = false;
+} else if(year % 4 === 0){
+    isLeapYear = true;
+}
+
+return isLeapYear;
+    //Y- leap year
+    //is it div by 100?
+        //Y - not a leap year
+        // is it div by 4?
+            //y - leap year
+            //n - not a leap year
 
 };
 
