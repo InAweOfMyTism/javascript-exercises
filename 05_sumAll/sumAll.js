4a2d0fa4029282c6033bff08bdde59eaf82fae7e
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-27 17:33:26 -0400

    add algorithm to loop across ints and add

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(input1,input2) {
+    if (){
+        return 'ERROR';
+    }
 
+    let total = 0;
+    let firstInt;
+    let lastInt;
+
+    for(let i = firstInt; i <= lastInt; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
