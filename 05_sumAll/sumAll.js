9bd4d1983e81623f7cd200949ba19b3f01218f92
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-27 18:05:10 -0400

    finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,16 @@
 const sumAll = function(input1,input2) {
-    if (){
+    if (!(Number.isInteger(input1) && Number.isInteger(input2)) || (input1 < 0 || input2 < 0)){
         return 'ERROR';
     }
 
     let total = 0;
-    let firstInt;
-    let lastInt;
+    let firstInt = input1;
+    let lastInt = input2;
+
+    if(input2 < input1){
+        firstInt = input2;
+        lastInt = input1;
+    }
 
     for(let i = firstInt; i <= lastInt; i++){
         total += i;
