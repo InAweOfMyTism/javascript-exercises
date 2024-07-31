b6cf2d7799b050844d63a5e570d9ec37d013d7bb
Chris Carroll <175367668+InAweOfMyTism@users.noreply.github.com>
2024-07-31 13:52:04 -0400

    add function skeleton and pseudocode

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let isPalindrome = false;
+    //filter out all non letter characters
+    //check odd/evenness of string length
+        //if odd, remove middle char
+    //cut in half and flip second half
+    //compare index by index
+    return isPalindrome;
 };
 
 // Do not edit below this line
